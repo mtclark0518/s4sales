f5c7112684e93a98cfee6090b68cfbff65f48469
mtclark0518 <mtclark0518@gmail.com>
2018-08-06 19:37:13 -0600

    rename data repo

--- S4Sales/Logging/data_repository.cs
+++ S4Sales/Logging/data_repository.cs
@@ -4,21 +4,12 @@ using S4Sales.Models;
 
 namespace S4Sales.Logging
 {
-    /// <Note>
-    // Holds logic for recording / logging site analytics
-    ///</Note>
-    public class S4Logger
+    public class DataRepository
     {
         private string _conn;
-        public S4Logger(IConfiguration config)
+        public DataRepository(IConfiguration config)
         {
             _conn = config["ConnectionStrings:tc_dev"];
-        }
-        public void Reimbursement(Reimbursement r)
-        {
-        }
-        public void Transaction(Purchase po)
-        {
         }
     }
 }
